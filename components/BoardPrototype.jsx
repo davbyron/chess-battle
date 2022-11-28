01c13825efc28b183da5f49868c2efd7278cb537
David Byron <davbyron@icloud.com>
2022-11-28 10:59:18 -0500

    update card component to dynamically retrieve data from mongodb

--- components/BoardPrototype.jsx
+++ components/BoardPrototype.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import BoardSquare from './BoardSquare'
 import Card from './Card'
 import styles from './BoardPrototype.module.css'
@@ -5,6 +6,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBolt, faChessPawn, faMountainSun } from '@fortawesome/free-solid-svg-icons'
 
 export default function BoardPrototype() {
+
+    const [player2Hand, setPlayer2Hand] = React.useState([]) // Init empty array
+
+    useEffect(() => {
+        console.log('you changed player 2\'s hand!')
+        console.log(player2Hand);
+    }, [player2Hand])
+
+    async function handlePawnDeckClick() {
+        // Get a random card 
+        const card = await fetch('http://localhost:3001/card');
+        const cardJSON = await card.json()
+
+        // Update hand
+        setPlayer2Hand(player2Hand.concat(cardJSON));
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.player1}>
@@ -33,7 +51,7 @@ export default function BoardPrototype() {
                 <div className={`player2-panel ${styles.middlegroundPanel}`}>
                     <div className={styles.decks}>
                         <div className={styles.deckContainer}>
-                            <div className={`${styles.deck} pawnDeck`}>
+                            <div className={`${styles.deck} pawnDeck`} onClick={handlePawnDeckClick}>
                                 <FontAwesomeIcon icon={faChessPawn} size='2x' />
                             </div>
                         </div>
@@ -61,8 +79,18 @@ export default function BoardPrototype() {
                     Exp(?): 0/10
                 </div>
                 <div className={styles.hand}>
-                    <Card />
-                    <Card />
+                    {player2Hand.map(card => {
+                        return (
+                            <Card
+                                name={card.name}
+                                text={card.ability}
+                                level={card.level}
+                                attack={card.attack}
+                                health={card.health}
+                                attackPattern={card.attack_pattern}
+                             />
+                        )
+                    })}
                 </div>
             </div>
         </div>
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faHandBackFist, faShield } from '@fortawesome/free-solid-svg-icons'
 import Pattern from './Pattern'
 import styles from './Card.module.css'
 
-export default function Card() {
+// import FlamingoImage from 'https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9&s=fb86e2e09fceac9b363af536b93a1275'
+
+export default function Card(props) {
+    const name = props.name;
+    const text = props.text == 'None' ? '' : props.text;
+    const level = props.level;
+    const attack = props.attack;
+    const health = props.health;
+    const attackPattern = props.attackPattern;
+
+    const cardLevelStyle = `level${level}Card`
 
     return (
         <div className={styles.container}>
-            <div className={styles.card} draggable="true">
-                <div className={styles.cardName}>
-                    Flaming Flamingo
-                </div>
+            <div className={`${styles.card} ${styles[cardLevelStyle]}`} draggable="true">
+                <div className={styles.cardName}>{name}</div>
                 <div className={styles.cardImage}>
+                    <Image src='https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9&s=fb86e2e09fceac9b363af536b93a1275' fill />
                     <div className={styles.cardLevel}> {/* This should be a component too -- for prototype just leaving as is */}
-                        1
+                        {level}
                     </div>
                 </div>
-                <div className={styles.cardText}>Heals one pawn in attack pattern instead of attacking</div>
+                <div className={styles.cardText}>{`${text}`}</div>
                 <div className={styles.patternAndStatsContainer}>
-                    <Pattern pattern='A' />
+                    <Pattern pattern={`${attackPattern}`} />
                     <div className={styles.stats}>
                         <div className={styles.attack}>
                             <FontAwesomeIcon icon={faHandBackFist} className={`${styles.faIcon} ${styles.attackIcon}`} />
-                            <div className={styles.attackNumber}>1</div>
+                            <div className={styles.attackNumber}>{attack}</div>
                         </div>
                         <div className={styles.defense}>
                             <FontAwesomeIcon icon={faShield} className={`${styles.faIcon} ${styles.defenseIcon}`} />
@@ -31,7 +41,7 @@ export default function Card() {
                         </div>
                         <div className={styles.health}>
                             <FontAwesomeIcon icon={faHeart} className={`${styles.faIcon} ${styles.healthIcon}`} />
-                            <div className={styles.healthNumber}>1</div>
+                            <div className={styles.healthNumber}>{health}</div>
                         </div>
                     </div>
                 </div>
