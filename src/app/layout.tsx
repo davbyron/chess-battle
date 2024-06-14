cc572f88cd7f7e55ddf414f1ab91f697a681928b
David Byron <davbyron@icloud.com>
2024-06-14 15:51:23 -0400

    remove fontawesome imports from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,6 @@ import { Provider } from 'react-redux'
 import store from '../store'
 import './global.css'
 
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
