12c87d813c0be4fd8ddcf338385e65367ab30739
David Byron <davbyron@icloud.com>
2024-06-14 11:37:25 -0400

    add custom text-stroke tailwind util

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      textStroke: {
+        '1': '0.05em black',
+        '2': '0.1em black',
+        '3': '0.15em black',
+      },
+    },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      textStroke: ['responsive', 'hover', 'focus'],
+    },
+  },
+  plugins: [
+    // Add custom utils
+    function({ addUtilities, theme, e }) {
+      const textStroke = theme('textStroke', {});
+
+      const utilities = Object.entries(textStroke).map(([key, value]) => {
+        return {
+          [`.${e(`text-stroke-${key}`)}`]: {
+            '-webkit-text-stroke': value,
+          },
+        };
+      });
+
+      addUtilities(utilities, ['responsive', 'hover', 'focus']);
+    },
+  ],
 }
