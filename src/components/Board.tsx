56585dc564cc244692c48f6ab125d7dd69bf9c4f
David Byron <davbyron@icloud.com>
2024-06-14 15:11:18 -0400

    rework player deck styles

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,11 +7,10 @@ import { useAppDispatch, useAppSelector } from '../hooks'
 import { numBoardSquares } from '../constants/board'
 import BoardSquare from './BoardSquare'
 import Hand from './Hand'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBolt, faChessPawn, faMountainSun } from '@fortawesome/free-solid-svg-icons'
+import { FaBolt, FaChessPawn, FaMountainSun } from 'react-icons/fa6'
 import { selectPlayer2Hand, addCardToPlayer2Hand } from '../slices/gameSlice'
 
-export default function Board(props) {
+export default function Board() {
   const dispatch = useAppDispatch()
   const player2Hand = useAppSelector(selectPlayer2Hand)
 
@@ -74,25 +73,22 @@ export default function Board(props) {
         <div className="h-full w-1/2 grid grid-cols-10 grid-rows-8">
           {boardSquares}
         </div>
-        <div className="w-1/4 flex flex-col justify-center items-center gap-10 border border-purple-800">
-          <div className="h-3/4 w-full flex items-center border border-pink-500">
-            <div className="w-1/3 h-3/4 flex items-center justify-center border border-orange-200">
-              <div className="flex items-center justify-center rounded-md bg-slate-300" onClick={handlePawnDeckClick}>
-                <FontAwesomeIcon icon={faChessPawn} size='2x' />
-              </div>
+        <div className="w-1/4 flex flex-col justify-center items-center gap-5 border border-purple-800">
+          <div className="flex flex-wrap gap-2 justify-center items-center border border-pink-500">
+            <div
+              className="h-[200px] w-[125px] flex items-center justify-center rounded-md bg-slate-300 hover:bg-slate-200 active:bg-slate-400 cursor-pointer"
+              onClick={handlePawnDeckClick}
+            >
+              <FaChessPawn className="text-3xl" />
             </div>
-            <div className="w-1/3 h-3/4 flex items-center justify-center border border-orange-200">
-              <div className="flex items-center justify-center rounded-md bg-slate-300">
-                <FontAwesomeIcon icon={faBolt} size='2x' />
-              </div>
+            <div className="h-[200px] w-[125px] flex items-center justify-center rounded-md bg-slate-300">
+              <FaBolt className="text-3xl" />
             </div>
-            <div className="w-1/3 h-3/4 flex items-center justify-center border border-orange-200">
-              <div className="flex items-center justify-center rounded-md bg-slate-300">
-                <FontAwesomeIcon icon={faMountainSun} size='2x' />
-              </div>
+            <div className="h-[200px] w-[125px] flex items-center justify-center rounded-md bg-slate-300">
+              <FaMountainSun className="text-3xl" />
             </div>
           </div>
-          <button className="w-1/2 h-1/6 bg-gray-400">
+          <button className="w-1/2 bg-gray-400">
             Next Turn {'>>>'}
           </button>
         </div>
