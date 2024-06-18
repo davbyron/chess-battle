52dd1672f6e4bdcd1760c6cc9d8f437ac530e39b
David Byron <davbyron@icloud.com>
2024-06-17 20:47:56 -0400

    add sign in with google to home page

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
                 hostname: '**.unsplash.com',
                 pathname: '**'
             },
+            {
+              protocol: 'https',
+              hostname: 'lh3.googleusercontent.com',
+              pathname: '**',
+            }
         ],
     },
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
 import Link from "next/link";
+import Image from "next/image";
+import { auth, signIn } from "src/auth";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
   return (
-    <div className="flex flex-col gap-10 items-center justify-center h-full">
+    <div className="flex flex-col gap-10 items-center h-full">
+      <nav className="w-full flex justify-between items-center py-2 px-5 bg-gray-100">
+        <h1 className="font-bold text-xl">Chess Battle</h1>
+        {session ? (
+          <div className="relative size-7 rounded-full">
+            <Image
+              src={session.user.image ?? ""}
+              fill
+              sizes="100%"
+              className="rounded-full"
+              alt={session.user.name ? `${session.user.name}'s profile image` : "User's profile image"}
+            />
+          </div>
+        ) : (
+          <form
+            action={async () => {
+              "use server"
+              await signIn("google")
+            }}
+          >
+            <button className="text-sm p-2 rounded-md bg-gray-400" type="submit">Signin with Google</button>
+          </form>
+        )}
+      </nav>
       Welcome to the game!
       <Link
         href="/game/test"
