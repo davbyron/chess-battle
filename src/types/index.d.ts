47d148626a75b5f8f6584b5115f556c559dc4ffa
David Byron <davbyron@icloud.com>
2024-06-19 14:27:43 -0400

    add 'id' to session

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,12 @@
+import NextAuth from 'next-auth';
+
 declare module "*.module.css";
+
+// Update NextAuth Session type
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
+}
