6ed6b714e647a536472036136463649251f964a9
David Byron <davbyron@icloud.com>
2024-06-24 16:50:23 -0400

    add drawCard procedure and remove old procedures

--- src/server/routers/game.ts
+++ src/server/routers/game.ts
@@ -1,33 +1,29 @@
 // Starter router.
 // Break up into more as app grows.
 import { publicProcedure, router } from "../trpc";
-import z from "zod";
 import { createApi } from "unsplash-js";
 import { UnsplashAccessKey } from "chess-battle.config";
+import { Card } from "src/types/types";
 
 export const gameRouter = router({
-  getAllCards: publicProcedure.query(async ({ ctx }) => {
+  drawCard: publicProcedure.mutation(async ({ ctx }) => {
     try {
       const cards = await ctx.prisma.card.findMany({});
-      return cards ?? [];
-    } catch (e) {
-      console.log(e)
-      return [];
-    }
-  }),
-  getCardPhotoUrl: publicProcedure.input(
-    z.object({
-      cardPhotoId: z.string(),
-    })
-  ).mutation(async ({ input }) => {
-    try {
+      const card = cards[Math.floor(Math.random() * cards.length)];
+
       const unsplash = createApi({ accessKey: UnsplashAccessKey });
-      const unsplashResponse = await unsplash.photos.get({ photoId: input.cardPhotoId });
+      const unsplashResponse = await unsplash.photos.get({ photoId: card.unsplashImgId });
       const cardPhotoUrl = unsplashResponse.response?.urls.regular;
-      return cardPhotoUrl;
+      
+      const modifiedCard: Card = {
+        ...card,
+        imgUrl: cardPhotoUrl ?? "",
+      };
+
+      return modifiedCard;
     } catch (e) {
       console.error(e);
-      return "";
+      return null;
     }
-  })
+  }),
 });
