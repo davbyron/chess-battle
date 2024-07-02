299f384e1c21d5cc130749ccc2d8b2f7e1ab6270
David Byron <davbyron@icloud.com>
2024-07-02 12:03:21 -0400

    use Deck and updated Hand component on game page

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -1,13 +1,9 @@
 import { io } from "socket.io-client";
-import { trpc } from "src/utils/trpc";
-
-import { useAppDispatch, useAppSelector } from "src/hooks";
-import { selectPlayerHand, addCardToPlayerHand } from "src/slices/gameSlice";
+import { auth } from "src/auth";
 
 import Board from "src/components/Board";
+import Deck from "src/components/Deck";
 import Hand from "src/components/Hand";
-
-import { FaBolt, FaChessPawn, FaMountainSun } from "react-icons/fa6"
 
 interface GameProps {
   params: {
@@ -15,14 +11,11 @@ interface GameProps {
   };
 }
 
-export default function Page(props: GameProps) {
+export default async function Page(props: GameProps) {
   const { params } = props;
   const { gameId } = params;
 
-  const dispatch = useAppDispatch()
-  const playerHand = useAppSelector(selectPlayerHand);
-
-  const drawCard = trpc.gameRouter.drawCard.useMutation();
+  const session = await auth();
 
   const socket = io("http://localhost:3001");
 
@@ -31,54 +24,23 @@ export default function Page(props: GameProps) {
     // TODO
   });
 
-  async function handlePawnDeckClick() {
-    try {
-      // Get a random card
-      const card = await drawCard.mutateAsync();
-      if (card) dispatch(addCardToPlayerHand(card));
-
-      socket.emit("draw-card", {
-        player: "1", // TODO
-        card: card,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   return (
     <div className="h-full w-full flex flex-col border border-black">
       <div className="h-1/4 border border-amber-400" />
       <div className="h-1/2 flex justify-center border border-green-500">
         <div className="w-1/4 flex flex-col justify-center items-center gap-10 border border-purple-800">
-          <div className="h-3/4 w-full flex items-center border border-pink-500">
-            <div className="w-1/3 h-3/4 flex items-center justify-center border border-orange-200">
-              <div className="flex items-center justify-center rounded-md bg-slate-300"></div>
-            </div>
-            <div className="w-1/3 h-3/4 flex items-center justify-center border border-orange-200">
-              <div className="flex items-center justify-center rounded-md bg-slate-300"></div>
-            </div>
-            <div className="w-1/3 h-3/4 flex items-center justify-center border border-orange-200">
-              <div className="flex items-center justify-center rounded-md bg-slate-300"></div>
-            </div>
+          <div className="flex flex-wrap gap-2 justify-center items-center border border-pink-500">
+            <Deck type="pawn" owner="opponent" />
+            <Deck type="event" owner="opponent" />
+            <Deck type="environment" owner="opponent" />
           </div>
-          <button className="w-1/2 h-1/6" style={{ visibility: 'hidden' }}></button>
         </div>
         <Board />
         <div className="w-1/4 flex flex-col justify-center items-center gap-5 border border-purple-800">
           <div className="flex flex-wrap gap-2 justify-center items-center border border-pink-500">
-            <div
-              className="h-[200px] w-[125px] flex items-center justify-center rounded-md bg-slate-300 hover:bg-slate-200 active:bg-slate-400 cursor-pointer"
-              onClick={handlePawnDeckClick}
-            >
-              <FaChessPawn className="text-3xl" />
-            </div>
-            <div className="h-[200px] w-[125px] flex items-center justify-center rounded-md bg-slate-300">
-              <FaBolt className="text-3xl" />
-            </div>
-            <div className="h-[200px] w-[125px] flex items-center justify-center rounded-md bg-slate-300">
-              <FaMountainSun className="text-3xl" />
-            </div>
+            <Deck type="pawn" owner="player" />
+            <Deck type="event" owner="player" />
+            <Deck type="environment" owner="player" />
           </div>
           <button className="w-1/2 bg-gray-400">
             Next Turn {'>>>'}
@@ -92,10 +54,7 @@ export default function Page(props: GameProps) {
           <br />
           Exp(?): 0/10
         </div>
-        <Hand
-          cards={playerHand}
-          playerId={2}
-        />
+        <Hand playerId={2} />
       </div>
     </div>
   );
