5921b3136d947f4b8bdf284bf74223794d6168e1
David Byron <davbyron@icloud.com>
2024-07-15 12:14:35 -0400

    add socket event types and clean up wsServer

--- src/server/wsServer.ts
+++ src/server/wsServer.ts
@@ -1,10 +1,20 @@
 // Use `npm run dev-wss` to start
 import { createServer } from "http";
 import { Server } from "socket.io";
+import {
+  ServerToClientEvents,
+  ClientToServerEvents,
+  InterServerEvents,
+  SocketData
+} from "src/types/types";
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
-  // options
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(httpServer, {
   cors: {
     origin: "*"
   }
@@ -13,12 +23,21 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   console.log("✅ Connected to Socket.IO.", socket.id);
 
-  socket.on("join-room", async (roomId) => {
-    await socket.join(roomId);
+  socket.on("joinRoom", async (roomId) => {
+    console.log("↙️ joinRoom");
+
+    const socketRooms = io.of("/").adapter.sids.get(socket.id);
+    if (!socketRooms?.has(roomId)) {
+      socket.join(roomId);
+    }
   })
 
-  socket.on("draw-card", (card) => {
-    socket.emit("draw-card", card);
+  socket.on("drawCard", (card, playerId, roomId) => {
+    console.log("↙️ drawCard");
+    console.log("↗️ drawCard");
+
+    // Use `io.to()...` if we need to let user who drew card also know that the event was emitted
+    socket.to(roomId).emit("drawCard", card, playerId);
   });
 
   socket.on("disconnect", (reason) => {
--- src/types/types.ts
+++ src/types/types.ts
@@ -13,3 +13,29 @@ export interface BoardSquareProps {
 export interface HandProps {
   player: "player" | "opponent"
 }
+
+/**
+ * SOCKET IO
+ */
+
+// Used when sending and broadcasting events
+export interface ServerToClientEvents {
+  drawCard: (card: Card, playerId: string) => void;
+}
+
+// Used when receiving events
+export interface ClientToServerEvents {
+  drawCard: (card: Card, playerId: string, roomId: string) => void;
+  joinRoom: (roomId: string) => void;
+}
+
+// Used for inter-server communication
+export interface InterServerEvents {
+  ping: () => void;
+}
+
+// Types the `socket.data` attribute
+export interface SocketData {
+  name: string;
+  age: number;
+}
