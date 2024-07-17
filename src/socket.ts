bdb703c0793cff5c971bdd27f19f03027038f08b
David Byron <davbyron@icloud.com>
2024-07-17 15:29:41 -0400

    add card to opponent's hand on draw

--- src/socket.ts
+++ src/socket.ts
@@ -2,10 +2,16 @@
 
 import { io, type Socket } from "socket.io-client";
 import { ServerToClientEvents, ClientToServerEvents } from "src/types/types";
+import { getSession } from "next-auth/react";
+import { dispatch, getState } from "src/store";
+import { addCardToOpponentHand } from "src/slices/gameSlice";
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io("http://localhost:3001");
 
-socket.on("drawCard", (card, playerId) => {
-  console.log("A card was drawn by playerId: ", playerId);
-  console.log("Card drawn: ", card);
+/**
+ * Adds a card to the opponent's hand
+ */
+socket.on("drawCard", async (card, playerId) => {
+  const session = await getSession();
+  if (session?.user.id !== playerId) dispatch(addCardToOpponentHand(card));
 })
