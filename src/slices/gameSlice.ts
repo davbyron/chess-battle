23d70080916b9ec450e6777538698dcd4096bc05
David Byron <davbyron@icloud.com>
2024-07-17 16:16:09 -0400

    format gameSlice

--- src/slices/gameSlice.ts
+++ src/slices/gameSlice.ts
@@ -4,69 +4,69 @@ import { RootState } from '../store'
 import { Card } from '../types/types'
 
 export interface GameState {
-    activeCard: Card | null
-    activeSquare: number | null
-    availableBoardSquares: number[] | null
-    cardIndex: number
-    playerHand: Card[],
-    opponentHand: Card[],
+  activeCard: Card | null
+  activeSquare: number | null
+  availableBoardSquares: number[] | null
+  cardIndex: number
+  playerHand: Card[],
+  opponentHand: Card[],
 }
 
 const initialState: GameState = {
-    activeCard: null,
-    activeSquare: null,
-    availableBoardSquares: null,
-    cardIndex: 0,
-    playerHand: [],
-    opponentHand: [],
+  activeCard: null,
+  activeSquare: null,
+  availableBoardSquares: null,
+  cardIndex: 0,
+  playerHand: [],
+  opponentHand: [],
 }
 
 export const gameSlice = createSlice({
-    name: 'game',
-    initialState: initialState,
-    reducers: {
-        activateBoardSquare: (state, action: PayloadAction<number>) => {
-            state.activeSquare = action.payload
-        },
-        deactivateBoardSquare: (state) => {
-            state.activeSquare = null
-        },
-        setAvailableBoardSquares: (state, action: PayloadAction<number[]>) => {
-            state.availableBoardSquares = action.payload
-        },
-        activateCard: (state, action: PayloadAction<Card>) => {
-            state.activeCard = action.payload
-        },
-        deactivateCard: (state) => {
-            state.activeCard = null
-        },
-        addCardToPlayerHand: (state, action:PayloadAction<Card>) => {
-          state.playerHand = state.playerHand.concat(action.payload)
-        },
-        removeCardFromPlayerHand: (state, action:PayloadAction<Card>) => {
-            const newHand = state.playerHand.filter(card => card.id !== action.payload.id)
-            state.playerHand = newHand
-        },
-        addCardToOpponentHand: (state, action:PayloadAction<Card>) => {
-            state.opponentHand = state.opponentHand.concat(action.payload)
-        },
-        removeCardFromOpponentHand: (state, action:PayloadAction<Card>) => {
-            const newHand = state.opponentHand.filter(card => card.id !== action.payload.id)
-            state.opponentHand = newHand
-        }
+  name: 'game',
+  initialState: initialState,
+  reducers: {
+    activateBoardSquare: (state, action: PayloadAction<number>) => {
+      state.activeSquare = action.payload;
+    },
+    deactivateBoardSquare: (state) => {
+      state.activeSquare = null;
+    },
+    setAvailableBoardSquares: (state, action: PayloadAction<number[]>) => {
+      state.availableBoardSquares = action.payload;
+    },
+    activateCard: (state, action: PayloadAction<Card>) => {
+      state.activeCard = action.payload;
+    },
+    deactivateCard: (state) => {
+      state.activeCard = null;
+    },
+    addCardToPlayerHand: (state, action:PayloadAction<Card>) => {
+      state.playerHand = state.playerHand.concat(action.payload);
+    },
+    removeCardFromPlayerHand: (state, action:PayloadAction<Card>) => {
+      const newHand = state.playerHand.filter(card => card.id !== action.payload.id);
+      state.playerHand = newHand;
+    },
+    addCardToOpponentHand: (state, action:PayloadAction<Card>) => {
+      state.opponentHand = state.opponentHand.concat(action.payload);
+    },
+    removeCardFromOpponentHand: (state, action:PayloadAction<Card>) => {
+      const newHand = state.opponentHand.filter(card => card.id !== action.payload.id);
+      state.opponentHand = newHand;
     }
+  }
 });
 
 export const {
-    activateCard,
-    deactivateCard,
-    activateBoardSquare,
-    deactivateBoardSquare,
-    setAvailableBoardSquares,
-    addCardToPlayerHand,
-    removeCardFromPlayerHand,
-    addCardToOpponentHand,
-    removeCardFromOpponentHand
+  activateCard,
+  deactivateCard,
+  activateBoardSquare,
+  deactivateBoardSquare,
+  setAvailableBoardSquares,
+  addCardToPlayerHand,
+  removeCardFromPlayerHand,
+  addCardToOpponentHand,
+  removeCardFromOpponentHand
 } = gameSlice.actions;
 
 export const selectActiveCard = (state: RootState) => state.game.activeCard;
